2ea49e8cc3c7401c49018a6b4eb1d58609d7cbe6
lyron08 <49680499+lyron08@users.noreply.github.com>
2024-08-07 13:55:34 +0800

    Initial Upload
    Initial upload

---
+++ src/views/HomeView.vue
@@ -0,0 +1,40 @@
+<template>
+    <header class="bg-[url('../assets/image/Meeting.jpg')] bg-cover bg-no-repeat bg-center h-[500px]">
+      <div class="bg-[#0f0b3ac6] h-full">
+      <nav class="flex py-5 h-[100px;] w-full bg-gradient-to-r from-[#46ffa514] to-[#00c19788] z-10">
+        <div class="flex wrapper">
+          <i class="bg-[url('../assets/image/Logo.svg')] bg-center bg-no-repeat bg-contain w-[75px] h-[75px]"></i> 
+          <h1 class="flex mt-3 ml-2 uppercase font-bold text-4xl text-[#24a388]"><a href="#">Anemo tech</a></h1>
+          <ul class="flex ml-auto [&_li]:mx-5 text-white text-2xl mt-3">
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Project</a></li>
+            <li><a href="#">Blogs</a></li>
+            <li><a href="#">About</a></li>
+          </ul>
+        </div>
+      </nav> 
+      <div class="flex wrapper relative items-center align-middle text-white [&_span]:uppercase" id="header-body">
+        <div class="w-[55%] mt-24">
+          <span class="block text-7xl font-bold">Top-Notch</span>
+          <span class="block text-7xl font-bold text-[#24a388]">Tech Services</span>
+          <p class="text-2xl">We take pride in  helping people from all walks of life achieve the service and happiness they deserver.</p>
+        </div>
+        <div class="bg-url('.at./assets/image/Station.svg') bg-center bg-no-repeat bg-cover w-[45%] h-[100px]">
+
+        </div>
+      </div>
+      </div>
+    </header>
+    
+    <main>
+      
+    </main>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
